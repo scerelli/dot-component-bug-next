82a5699821337944ca99f5963b7065006fbbd5cb
scerelli <hi@stefanocerelli.com>
2023-11-22 17:58:14 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Select } from '../components/select';
+
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return (
+    <Select>
+      <Select.Item value="value">Things</Select.Item>
+    </Select>
+  )
+}
---
+++ components/select.tsx
@@ -0,0 +1,166 @@
+'use client'
+import { createContext, forwardRef, useCallback, useContext } from 'react'
+import type { FC, ReactElement, ReactNode } from 'react'
+import * as RadixSelect from '@radix-ui/react-select'
+
+
+export type SelectContext = {
+  size: SelectSizes
+}
+const SelectContext = createContext<SelectContext | null>(null)
+const useSelect = () => {
+  const context = useContext(SelectContext)
+  if(!context) throw new Error('Select context not found in parent.')
+
+  return context
+}
+
+type SelectLabelProps = RadixSelect.SelectLabelProps
+const SelectLabel: FC<SelectLabelProps> = (props) => {
+  const context = useSelect()
+  return (
+    <RadixSelect.Label {...props} asChild>
+      <label>
+        {props.children}
+      </label>
+    </RadixSelect.Label>
+  )
+}
+
+type SelectGroupProps = RadixSelect.SelectGroupProps
+const SelectGroup: FC<SelectGroupProps> = ({ className, ...props }) => {
+  return (
+    <RadixSelect.Group {...props} />
+  )
+}
+
+export interface SelectComponent extends React.ForwardRefExoticComponent<React.RefAttributes<HTMLDivElement> & SelectProps> {
+  Item: React.FC<SelectItemProps>
+  Group: React.FC<SelectGroupProps>
+  Label: React.FC<SelectLabelProps>
+}
+
+export enum SelectSizes {
+  lg = 'lg',
+  md = 'md',
+  sm = 'sm'
+}
+
+export interface SelectProps extends Omit<RadixSelect.SelectTriggerProps, 'onChange' | 'value' | 'defaultValue' > {
+  trigger?: ReactNode
+  className?: string
+  children: ReactElement<typeof Item> | ReactElement<typeof Item>[]
+  size?: keyof typeof SelectSizes
+  placeholder?: string
+  triggerProps?: RadixSelect.SelectTriggerProps
+  errorMessage?: string
+  label?: string
+  onChange?: (value: string | undefined) => void
+  value?: string
+  defaultValue?: string
+}
+
+export type SelectItemProps = any
+const Item: FC<SelectItemProps> = ({ children, icon, disabled, iconPosition, ...otherProps }) => {
+  return (
+    <RadixSelect.Item {...otherProps}>
+      <div>
+        <div>
+          <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
+          <RadixSelect.ItemIndicator>
+            icon
+          </RadixSelect.ItemIndicator>
+        </div>
+      </div>
+    </RadixSelect.Item>
+  )
+}
+
+const SelectBase = forwardRef<HTMLButtonElement, SelectProps>(({
+  className,
+  trigger,
+  children,
+  size = SelectSizes.md,
+  errorMessage,
+  label,
+  id,
+  placeholder,
+  onChange,
+  value,
+  defaultValue,
+  ...props
+}, forwardedRef) => {
+
+  const handleChange = useCallback((value: string) => {
+    if(!props.disabled && onChange) onChange(value)
+  }, [props.disabled, onChange])
+
+  return (
+    <SelectContext.Provider value={{
+      size: SelectSizes[size]
+    }}>
+        <RadixSelect.Root onValueChange={handleChange} value={value} defaultValue={defaultValue} >
+        <div>
+        {label
+        ? (
+          <label
+            htmlFor={id}
+            aria-label={props['aria-label'] ? props['aria-label'] : label}
+          >
+            {label}
+          </label>
+          )
+        : null}
+          <RadixSelect.Trigger
+            id={id}
+            ref={forwardedRef}
+            data-trigger-size={size}
+            data-trigger-error={Boolean(errorMessage)}
+            data-trigger-disabled={props.disabled}
+            aria-label={props['aria-label'] ? props['aria-label'] : label}
+            {...props}
+          >
+              <span>
+                <RadixSelect.Value placeholder={placeholder} />
+              </span>
+              <RadixSelect.Icon>
+                icon
+              </RadixSelect.Icon>
+          </RadixSelect.Trigger>
+          {errorMessage
+          ? (
+            <span
+              data-text-size={size}
+            >
+              {errorMessage}
+            </span>
+            )
+          : null}
+          </div>
+          <RadixSelect.Portal>
+            <RadixSelect.Content position='popper'>
+              <RadixSelect.ScrollUpButton>
+                up
+              </RadixSelect.ScrollUpButton>
+              <RadixSelect.Viewport>
+                {children}
+              </RadixSelect.Viewport>
+              <RadixSelect.ScrollDownButton>
+                down
+              </RadixSelect.ScrollDownButton>
+            </RadixSelect.Content>
+          </RadixSelect.Portal>
+        </RadixSelect.Root>
+    </SelectContext.Provider>
+  )
+})
+
+SelectBase.displayName = 'SelectBase'
+
+export const Select = SelectBase as any as SelectComponent
+
+Select.Item = Item
+Select.Label = SelectLabel
+Select.Group = SelectGroup
+
+Select.displayName = 'Select'
